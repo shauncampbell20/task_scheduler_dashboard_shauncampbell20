24111b4c28bd538abe29990de0098628342e69cd
shaunyboy20 <44283630+shaunyboy20@users.noreply.github.com>
2024-11-05 09:17:37 -0600

    Update config.py

--- src/task_scheduler_dashboard_shauncampbell20/config.py
+++ src/task_scheduler_dashboard_shauncampbell20/config.py
@@ -29,35 +29,7 @@ def list_configs():
     for k, v in configs.items():
         print(k,":",v)
 
-
-def build(update=True):
-    ## Main function for building and updated the database
-    with open(os.path.join(_loc, 'config.json'), 'r') as config:
-        configs = json.load(config)
-    PROCESS_AUTOMATION_HOME = configs['PROCESS_AUTOMATION_HOME']
-    SCHEDULER_FOLDER = configs['SCHEDULER_FOLDER']
-    DB_NAME = configs["DB_NAME"]
-    
-    # Create directory PROCESS_AUTOMATION_HOME if doesn't exist
-    if not os.path.exists(PROCESS_AUTOMATION_HOME):
-        os.mkdir(PROCESS_AUTOMATION_HOME)
-        warnings.warn('Created directory '+PROCESS_AUTOMATION_HOME)
-    
-    # Create logs directory in PROCESS_AUTOMATION_HOME if doesn't exist
-    process_automation_logs = os.path.join(PROCESS_AUTOMATION_HOME, 'logs')
-    if not os.path.exists(process_automation_logs):
-        os.mkdir(process_automation_logs)
-    process_automation_db = os.path.join(PROCESS_AUTOMATION_HOME, DB_NAME)
-    
-    # Create Executors and Tasks tables. If update is true, it will clear and reset these tables to be up to date
-    # If update = False, i.e. initializing for the first time, it will also create the Runs table.
-    if not os.path.exists(os.path.join(PROCESS_AUTOMATION_HOME, DB_NAME)):
-        update = False
-
-    local = sqlite3.connect(os.path.join(PROCESS_AUTOMATION_HOME, DB_NAME))
-    cursor = local.cursor()
-
-    # --- Get Task Scheduler information ---#
+def parse_task_scheduler(SCHEDULER_FOLDER):
     import win32com.client
     import pythoncom
     TASK_ENUM_HIDDEN = 1
@@ -95,44 +67,118 @@ def build(update=True):
                 d[taskName]['Last Result'] = resultCodes[task.LastTaskResult]
             except KeyError:
                 d[taskName]['Last Result'] = task.LastTaskResult
+            d[taskName]['Machine'] = os.environ['COMPUTERNAME']
+    return d
 
-    # --- Create Executors Table ---#
-    cursor.execute('''DROP TABLE IF EXISTS Executors;''')
-    cursor.execute('''
-    CREATE TABLE Executors (
-    name VARCHAR,
-    state VARCHAR,
-    next_run_time VARCHAR,
-    last_run_time VARCHAR,
-    last_run_result VARCHAR,
-    hidden VARCHAR,
-    command VARCHAR,
-    folder VARCHAR
-    )''')
+def build(update=True):
+    ## Main function for building and updated the database
+    
+    def create_run_table():
+        # Create run table if it does not exist
+        if ('Runs',) not in cursor.execute('''SELECT name FROM sqlite_master WHERE type = 'table' ''').fetchall():
+            cursor.execute('''DROP TABLE IF EXISTS Runs''')
+            cursor.execute('''
+            CREATE TABLE Runs (
+            run_id INTEGER PRIMARY KEY,
+            script_id VARCHAR,
+            log_file VARCHAR,
+            start_time VARCHAR,
+            end_time VARCHAR,
+            records INT,
+            result VARCHAR,
+            errors INT,
+            warnings INT,
+            user VARCHAR,
+            machine VARCHAR
+            )
+            ''')
+            local.commit()
+        else:
+            warnings.warn('Runs table exists, skipping create run table')
+        
+    def create_executors_table():
+        # Creates Executors table
+        cursor.execute('''DROP TABLE IF EXISTS Executors;''')
+        cursor.execute('''
+        CREATE TABLE Executors (
+        name VARCHAR,
+        state VARCHAR,
+        next_run_time VARCHAR,
+        last_run_time VARCHAR,
+        last_run_result VARCHAR,
+        hidden VARCHAR,
+        command VARCHAR,
+        folder VARCHAR,
+        machine VARCHAR
+        )''')
+        local.commit()
+        
+    def create_tasks_table():
+        # Creates Tasks table
+        cursor.execute('''DROP TABLE IF EXISTS Tasks;''')
+        cursor.execute('''
+        CREATE TABLE Tasks (
+        script_id VARCHAR,
+        command VARCHAR,
+        script VARCHAR,
+        run_dir VARCHAR,
+        execution_command VARCHAR,
+        machine VARCHAR
+        )''')
+        
+    # Load configs
+    with open(os.path.join(_loc, 'config.json'), 'r') as config:
+        configs = json.load(config)
+    PROCESS_AUTOMATION_HOME = configs['PROCESS_AUTOMATION_HOME']
+    SCHEDULER_FOLDER = configs['SCHEDULER_FOLDER']
+    DB_NAME = configs["DB_NAME"]
+    machine = os.environ['COMPUTERNAME']
+    
+    # Create directory PROCESS_AUTOMATION_HOME if doesn't exist
+    if not os.path.exists(PROCESS_AUTOMATION_HOME):
+        os.mkdir(PROCESS_AUTOMATION_HOME)
+        warnings.warn('Created directory '+PROCESS_AUTOMATION_HOME)
+    
+    # Create logs directory in PROCESS_AUTOMATION_HOME if doesn't exist
+    process_automation_logs = os.path.join(PROCESS_AUTOMATION_HOME, 'logs')
+    if not os.path.exists(process_automation_logs):
+        os.mkdir(process_automation_logs)
+    
+    # If update = False, i.e. initializing for the first time, create the Runs, Executors, and Tasks tables.
+    if not os.path.exists(os.path.join(PROCESS_AUTOMATION_HOME, DB_NAME)):
+        update = False
+    local = sqlite3.connect(os.path.join(PROCESS_AUTOMATION_HOME, DB_NAME))
+    cursor = local.cursor()
+    if not update:
+        create_run_table()
+        create_executors_table()
+        create_tasks_table()
+    else:
+        if ('Runs',) not in cursor.execute('''SELECT name FROM sqlite_master WHERE type = 'table' ''').fetchall():
+            create_run_table()
+        if ('Executors',) not in cursor.execute('''SELECT name FROM sqlite_master WHERE type = 'table' ''').fetchall():
+            create_executors_table()
+        if ('Tasks',) not in cursor.execute('''SELECT name FROM sqlite_master WHERE type = 'table' ''').fetchall():
+            create_tasks_table()
 
-    # --- Insert Task Scheduler Information into Executors Table ---#
+    # Get Task Scheduler information
+    d = parse_task_scheduler(SCHEDULER_FOLDER)
+
+    # Insert Task Scheduler Information into Executors table if task is not disabled
+    cursor.execute(f'''DELETE FROM Executors WHERE machine = '{machine}' ''')
     for pname in d.keys():
-        cursor.execute(f'''
-        INSERT INTO Executors 
-        VALUES ('{pname}', '{d[pname]['State']}', '{str(d[pname]['Next Run'])}', 
-        '{str(d[pname]['Last Run'])}', '{d[pname]['Last Result']}', 
-        '{d[pname]['Hidden']}', '{d[pname]['Command']}', '{d[pname]['Folder']}')
-        ''')
+        if d[pname]['State'] != 'Disabled':
+            cursor.execute(f'''
+            INSERT INTO Executors 
+            VALUES ('{pname}', '{d[pname]['State']}', '{str(d[pname]['Next Run'])}', 
+            '{str(d[pname]['Last Run'])}', '{d[pname]['Last Result']}', 
+            '{d[pname]['Hidden']}', '{d[pname]['Command']}', '{d[pname]['Folder']}', '{d[pname]['Machine']}')
+            ''')
     local.commit()
 
-    # --- Create and Populate Tasks Table ---#
-    cursor.execute('''DROP TABLE IF EXISTS Tasks;''')
-    cursor.execute('''
-    CREATE TABLE Tasks (
-    script_id VARCHAR,
-    command VARCHAR,
-    script VARCHAR,
-    run_dir VARCHAR,
-    execution_command VARCHAR
-    )''')
-    
-    # Parse batch files
-    df = pd.read_sql_query('''SELECT * FROM Executors ''', local)
+    # Parse batch files and insert into Tasks table
+    cursor.execute(f'''DELETE FROM Tasks WHERE machine = '{machine}' ''')
+    df = pd.read_sql_query(f'''SELECT * FROM Executors WHERE machine = '{machine}' ''', local)
     for batchFile, taskfolder in zip(df['command'], df['folder']):
         try:
             with open(batchFile, 'r') as f:
@@ -150,43 +196,15 @@ def build(update=True):
             for tname in tasks.keys():
                 cursor.execute(f'''
                 INSERT INTO Tasks 
-                VALUES ('{tasks[tname]['script_id']}', '{tasks[tname]['command']}', '{tname}', '{tasks[tname]['run_dir']}', '{tasks[tname]['execution_command']}')
+                VALUES ('{tasks[tname]['script_id']}', '{tasks[tname]['command']}', '{tname}', '{tasks[tname]['run_dir']}', '{tasks[tname]['execution_command']}', '{machine}')
                 ''')
             local.commit()
         except Exception as e:
             warnings.warn('Exception in '+batchFile+': '+str(e))
     local.commit()
     
-    def create_run_table():
-        cursor.execute('''DROP TABLE IF EXISTS Runs''')
-        cursor.execute('''
-        CREATE TABLE Runs (
-        run_id INTEGER PRIMARY KEY,
-        script_id VARCHAR,
-        log_file VARCHAR,
-        start_time VARCHAR,
-        end_time VARCHAR,
-        records INT,
-        result VARCHAR,
-        errors INT,
-        warnings INT,
-        user VARCHAR,
-        machine VARCHAR
-        )
-        ''')
-        local.commit()
-    
-    if not update:
-        # --- Create Runs Table ---#
-        # --- Does not run if update is true ---#
-        create_run_table()
-    else:
-        if ('Runs',) not in cursor.execute('''SELECT name FROM sqlite_master WHERE type = 'table' ''').fetchall():
-            create_run_table()
-            
-
-    # --- Add tasks that trigger other tasks to Executors table ---#
-    pd.read_sql_query('''
+    # Add tasks that trigger other tasks to Executors table
+    pd.read_sql_query(f'''
     SELECT DISTINCT
     t1.command as name,
     'Ready' as state,
@@ -194,7 +212,8 @@ def build(update=True):
     (SELECT end_time FROM Runs WHERE script_id = t2.script_id ORDER BY end_time DESC) as last_run_time,
     (SELECT result FROM Runs WHERE script_id = t2.script_id ORDER BY end_time DESC ) as last_run_result,
     'False' as hidden,
-    t1.command as command
+    t1.command as command,
+    '{machine}' as machine
     FROM Tasks t1 
     LEFT JOIN Tasks t2 ON t1.command = t2.script_id 
     WHERE t1.command NOT IN (SELECT command FROM Executors)
@@ -211,6 +230,7 @@ if __name__ == "__main__":
     parser.add_argument('--dbname', type=str)
     parser.add_argument('--list', '-l' , action='store_true')
     parser.add_argument('--build', '-b' , action='store_true')
+    parser.add_argument('--reset', '-r' , action='store_true')
     args = parser.parse_args()
     if args.home:
         set_config('PROCESS_AUTOMATION_HOME', args.home)
@@ -220,6 +240,8 @@ if __name__ == "__main__":
         set_config('DB_NAME', args.dbname)
     if args.list:
         list_configs()
-    if args.build:
-        build()
+    if args.reset:
+        build(update=False)
+    elif args.build:
+        build(update=True)
         
